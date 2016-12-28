77a8b24a4b10aaee50e9c7d7aee1caa523e65067
Paul Straw <paulstraw@paulstraw.com>
2016-12-28 08:40:18 -0800

    Add a monkey

---
+++ monkey/index.js
@@ -0,0 +1,34 @@
+(function() {
+  const TARGET = 'It was the best of times, it was the blurst of times.';
+  const LETTERS = 'abcdefghijklmnopqrstuvwxyz ,. ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  const LETTER_LENGTH = LETTERS.length;
+  const DISPLAY = document.querySelector('.display');
+  var input = '';
+
+  function addCharacter() {
+    input += LETTERS[~~(Math.random() * LETTER_LENGTH)];
+  }
+
+  function render() {
+    DISPLAY.innerText = input;
+  }
+
+  function checkValidity() {
+    return input === TARGET.substr(0, input.length);
+  }
+
+  function run() {
+    addCharacter();
+    render();
+
+    if (!checkValidity()) {
+      input = input.substr(0, input.length - 1);
+    }
+
+    if (input !== TARGET) {
+      setTimeout(run, 0);
+    }
+  }
+
+  run();
+}());
